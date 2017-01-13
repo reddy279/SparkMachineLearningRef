767a570c6be383610ca095a5bc9a8591e8cfe9ee
Shashidhar Reddy <reddy279@gmail.com>
2017-01-12 22:48:45 -0500

    Updated UberCluster

--- src/main/scala-2.11/com/uber/ml/ml/cluster/analysis/uc1/UberCluster.scala
+++ src/main/scala-2.11/com/uber/ml/ml/cluster/analysis/uc1/UberCluster.scala
@@ -5,6 +5,11 @@ import org.apache.spark.ml.clustering.KMeans
 import org.apache.spark.ml.feature.VectorAssembler
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.types.{StringType, _}
+import org.apache.spark._
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql._
 
 /**
   * Created by p5103951 on 1/10/17.
@@ -40,7 +45,7 @@ object UberCluster extends App {
   // These features are transformed into Feature Vectors of the numbers  .
 
 
-  val feature_cols= Array("lat", "lon")
+  val feature_cols = Array("lat", "lon")
 
   // Set the input column names to the feature Vectore
 
@@ -48,12 +53,12 @@ object UberCluster extends App {
   val assembler = new VectorAssembler().setInputCols(feature_cols).setOutputCol("features")
 
   //new dataframe with all the features in the vector column
-  val transferred_df= assembler.transform(uber_ds_df)
+  val transferred_df = assembler.transform(uber_ds_df)
 
   // randomly splitting the data for traning and test
-  val Array(trainingData, testData)=  transferred_df.randomSplit(Array(0.7,0.3),5043)
+  val Array(trainingData, testData) = transferred_df.randomSplit(Array(0.7, 0.3), 5043)
 
-  val kmean= new KMeans().setK(8).setFeaturesCol("features").setPredictionCol("predictions")
+  val kmean = new KMeans().setK(8).setFeaturesCol("features").setPredictionCol("predictions")
 
   //building the model
   val model = kmean.fit(transferred_df)
@@ -61,12 +66,32 @@ object UberCluster extends App {
   //save the oput put to path
   //model.save("")
 
-  model.clusterCenters.foreach(println)
+  //model.clusterCenters.foreach(println)
+
+  //We use the model to test and analyse the cluster further
+
+  val categories = model.transform(testData)
+
+  /**
+    * Problem Statement 1:
+    * -------------------
+    * Which hours of the day and which cluster had the highest number of pickups.
+    *
+    */
+
+  import spark.sqlContext.implicits._
+
+  //Month
+  categories.select(month($"dt").alias("month"), dayofmonth($"dt").alias("day"), hour($"dt").alias("hour"), $"predictions").groupBy("month", "day", "hour", "predictions").agg(count("predictions").alias("count")).orderBy("day", "hour", "predictions").show
 
 
+  //Hour
+  categories.select(hour($"dt").alias("hour"), $"predictions").groupBy("hour", "prediction").agg(count("predictions")
+    .alias("count")).orderBy(desc("count")).show
 
 
-
-
-
+  /**
+    * How Many pickups occured in each cluster.
+    */
+  categories.groupBy("predictions").count().show()
 }
